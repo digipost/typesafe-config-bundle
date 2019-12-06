fc856b1cebf14cb6a9ee3ecc69004351d1023694
Rune Flobakk <flobakk@gmail.com>
2019-12-06 16:26:21 +0100

    Stylize a bit more like a JUnit 5 test

--- src/test/java/no/digipost/dropwizard/ConfigurationTest.java
+++ src/test/java/no/digipost/dropwizard/ConfigurationTest.java
@@ -38,46 +38,43 @@ import static no.digipost.dropwizard.TypeSafeConfigurationFactory.SECRET_KEY;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class ConfigurationTest {
+class ConfigurationTest {
 
-    final Validator validator;
-    final ObjectMapper objectMapper;
-    final ConfigurationFactory<TestConfig> configFactory;
+    private static final Validator validator = Validation.byProvider(HibernateValidator.class).configure()
+                                                  .buildValidatorFactory().getValidator();
+    private static final ObjectMapper objectMapper = Jackson.newObjectMapper();
 
-    public ConfigurationTest() {
-        validator = Validation.byProvider(HibernateValidator.class)
-                .configure()
-                .buildValidatorFactory().getValidator();
-        objectMapper = Jackson.newObjectMapper();
-        configFactory = new TypeSafeConfigurationFactory<>(TestConfig.class, validator, objectMapper, "dw");
-    }
+    private final ConfigurationFactory<TestConfig> configFactory =
+        new TypeSafeConfigurationFactory<>(TestConfig.class, validator, objectMapper, "dw");
+    private final ConfigurationSourceProvider configSourceProvider =
+        new ConfigurationSourceProviderWithFallback(new FileConfigurationSourceProvider(), new ResourceConfigurationSourceProvider());
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         System.setProperty("driverClassSystemProperty", "driverClassFromSystemProperty");
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         System.clearProperty(ENV_KEY);
         System.clearProperty(SECRET_KEY);
         System.clearProperty("driverClassSystemProperty");
     }
 
     @Test
-    public void should_load_local_config_without_errors() throws IOException, ConfigurationException {
+    void should_load_local_config_without_errors() throws IOException, ConfigurationException {
         setEnv("local");
         configFactory.build(configSourceProvider, "test-config.yml");
     }
 
     @Test
-    public void should_load_test_config_without_errors() throws IOException, ConfigurationException {
+    void should_load_test_config_without_errors() throws IOException, ConfigurationException {
         setEnv("test");
         configFactory.build(configSourceProvider, "test-config.yml");
     }
 
     @Test
-    public void should_use_default_values_when_environment_has_no_specific_value() throws IOException, ConfigurationException {
+    void should_use_default_values_when_environment_has_no_specific_value() throws IOException, ConfigurationException {
         setEnv("local");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
@@ -85,7 +82,7 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_override_defaults_with_environment_specific_values() throws IOException, ConfigurationException {
+    void should_override_defaults_with_environment_specific_values() throws IOException, ConfigurationException {
         setEnv("test");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
@@ -93,7 +90,7 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_allow_empty_default_with_environment_specific_value() throws IOException, ConfigurationException {
+    void should_allow_empty_default_with_environment_specific_value() throws IOException, ConfigurationException {
         setEnv("test");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
@@ -101,7 +98,7 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_load_secret_config_from_file() throws IOException, ConfigurationException {
+    void should_load_secret_config_from_file() throws IOException, ConfigurationException {
         setEnv("local");
         System.setProperty(TypeSafeConfigurationFactory.SECRET_KEY, getClass().getResource("/test-secret.yml").getFile());
         final TestConfig config =
@@ -110,7 +107,7 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_interpolate_variables() throws IOException, ConfigurationException {
+    void should_interpolate_variables() throws IOException, ConfigurationException {
         setEnv("test");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
@@ -118,7 +115,7 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_interpolate_variables_with_overrides() throws IOException, ConfigurationException {
+    void should_interpolate_variables_with_overrides() throws IOException, ConfigurationException {
         setEnv("local");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
@@ -126,17 +123,15 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void should_resolve_system_properties() throws IOException, ConfigurationException {
+    void should_resolve_system_properties() throws IOException, ConfigurationException {
         setEnv("test2");
         final TestConfig config =
                 configFactory.build(configSourceProvider, "test-config.yml");
         assertThat(config.database.getDriverClass(), is("driverClassFromSystemProperty"));
     }
 
-    private void setEnv(final String env) {
+    private static void setEnv(final String env) {
         System.setProperty(ENV_KEY, env);
     }
 
-    private final ConfigurationSourceProvider configSourceProvider =
-            new ConfigurationSourceProviderWithFallback(new FileConfigurationSourceProvider(), new ResourceConfigurationSourceProvider());
 }
